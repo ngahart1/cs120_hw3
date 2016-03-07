9d368de2d62d86ce70851a1a352646cfa55d5676
Neil Gahart <ngahart1@ugradx.cs.jhu.edu>
2016-03-07 17:33:57 -0500

    functionality added for do_c and do_t

--- hw3.c
+++ hw3.c
@@ -1,8 +1,10 @@
 //Neil Gahart
-
+#include <ctype.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "linked_list.h"
+#include "menu_options.h"
 
 int main(int argc, char *argv[]){
   FILE *input = fopen(argv[1],"r");
@@ -11,7 +13,7 @@ int main(int argc, char *argv[]){
     return 1;
   }
   int current; //arbitrary initializer
-  // struct node courses; //this will be the linked list of courses
+  node *head = {NULL, NULL}; //this will be the linked list of courses
   int counter = 0;
   char line[47];
   while (1){
@@ -21,7 +23,18 @@ int main(int argc, char *argv[]){
       } //end if
       else if (current == '\n'){
 	line[counter]='\0';
-	printf("%s \n", line);
+	//printf("%s \n", line);
+	node *new = new_node(line);
+	if (head == NULL){
+	  head = new;
+	}//end if
+	else {
+	  node *temp = head;
+	  while (temp->next != NULL){
+	    temp = temp->next;
+	  }
+	  temp->next = new;
+	} //end else
 	memset(&line[0], 0, sizeof(line));
 	counter = 0;
       } //end else if
@@ -30,6 +43,28 @@ int main(int argc, char *argv[]){
 	line[counter] = current;
 	counter++;
       } //end else
-  }//end first while
-  
+  }//end while
+
+  char choice;
+  while(1) {
+    printf("Please enter your choice: ");
+    scanf("%c", &choice);
+    choice = toupper(choice);
+    if (choice == 'P'){
+      do_p(head);
+    }//end if
+    else if (choice == 'C') {
+      do_c(head);
+    }
+    else if (choice == 'T') {
+      do_t(head);
+    }
+    else if (choice == 'Q') {
+      return 0;
+    }
+    else {
+      puts("Invalid input");
+    }
+      
+  } //end while
 }//end main
---
+++ menu_options.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include "menu_options.h"
+#include "linked_list.h"
+
+void do_p (struct node *cur){
+  //Prints all courses in the catalog
+  while (cur != NULL){
+    print_course(cur);
+    cur = cur->next;
+  } //end while
+} //end do_p
+
+void do_c (struct node *cur) {
+  //Print specified course only
+  printf("Please enter the course identifier (xx.ddd.nnn): ");
+  char identifier[11];
+  scanf("%s", identifier);
+  char division[3] = {identifier[0], identifier[1], '\0'};
+  int department = 100*(identifier[3]-48) + 10*(identifier[4]-48) + (identifier[5]-48);
+  int number = 100*(identifier[7]-48) + 10*(identifier[8]-48) + (identifier[9]-48);
+  int found = 0;
+  while (!found && cur != NULL) {
+    int curdept = cur->c.department;
+    int curnumber = cur->c.number;
+    if (strcmp(cur->c.division, division) == 0 && curdept==department && curnumber==number) {
+      printf("%1.1f %s\n", cur->c.credit, cur->c.name);
+      found = 1;
+    }//end if
+    cur = cur->next;
+  }//end while
+  if (!found){
+    puts("Absent");
+  }//end if
+} //end do_c
+
+void do_t (struct node *cur) {
+  printf("Please enter the course identifier (xx.ddd.nnn): ");
+  char identifier[11];
+  scanf("%s", identifier);
+  char division[3] = {identifier[0], identifier[1], '\0'};
+  int department = 100*(identifier[3]-48) + 10*(identifier[4]-48) + (identifier[5]-48);
+  int number = 100*(identifier[7]-48) + 10*(identifier[8]-48) + (identifier[9]-48);
+
+  printf("Please enter the desired course title (32 character maximum): ");
+  char new_title[33];
+  scanf("%s", new_title);
+
+  int found = 0;
+  while (!found && cur != NULL){
+    int curdept = cur->c.department;
+    int curnumber = cur->c.number;
+    if (strcmp(cur->c.division, division) == 0 && curdept==department && curnumber==number) {
+      strcpy(cur->c.name, new_title);
+      found=1;
+    }//end if
+    cur = cur->next;
+  }//end while
+  if (found) {puts("Updated");}
+  else {puts("Absent");}
+} //end do_t
