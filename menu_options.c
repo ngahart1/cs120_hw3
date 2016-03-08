6549200a1c167327e0da18be7ae1b4f341a02625
Neil Gahart <ngahart1@ugradx.cs.jhu.edu>
2016-03-08 16:44:52 -0500

    do_t accepts spaces in strings

--- menu_options.c
+++ menu_options.c
@@ -42,9 +42,10 @@ void do_t (struct node *cur) {
   int department = 100*(identifier[3]-48) + 10*(identifier[4]-48) + (identifier[5]-48);
   int number = 100*(identifier[7]-48) + 10*(identifier[8]-48) + (identifier[9]-48);
 
+  char dummy = scanf("%c", &dummy);
   printf("Please enter the desired course title (32 character maximum): ");
   char new_title[33];
-  scanf("%s", new_title);
+  scanf("%[^\n]", new_title);
 
   int found = 0;
   while (!found && cur != NULL){
@@ -59,3 +60,29 @@ void do_t (struct node *cur) {
   if (found) {puts("Updated");}
   else {puts("Absent");}
 } //end do_t
+
+void do_r (struct node *cur) {
+  printf("Please enter the course identifier (xx.ddd.nnn): ");
+  char identifier[11];
+  scanf("%s", identifier);
+  char division[3] = {identifier[0], identifier[1], '\0'};
+  int department = 100*(identifier[3]-48) + 10*(identifier[4]-48) + (identifier[5]-48);
+  int number = 100*(identifier[7]-48) + 10*(identifier[8]-48) + (identifier[9]-48);
+
+  printf("Please enter the new credit amount: ");
+  float new_credit;
+  scanf("%f", &new_credit);
+
+  int found = 0;
+  while (!found && cur != NULL){
+    int curdept = cur->c.department;
+    int curnumber = cur->c.number;
+    if (strcmp(cur->c.division, division) == 0 && curdept==department && curnumber==number) {
+      cur->c.credit = new_credit;
+      found=1;
+    }//end if
+    cur = cur->next;
+  }//end while
+  if (found) {puts("Updated");}
+  else {puts("Absent");}
+}//end do_r 
